73c72ce4c895f479ad60e6c5cc5c8c86ba643b33
wallis999 <wallis999@de8bf3d2-1df4-11de-9c9a-2d325a3e6494>
2009-07-13 09:01:36 +0000

    对那些只抓到了学校名却无法抓到十大内容的学校进行过滤

--- BBS_PARSE/model/models.py
+++ BBS_PARSE/model/models.py
@@ -9,43 +9,44 @@ from django import shortcuts
 
 
 class HighSchoolBbs(db.Model):
-    schoolname      = db.StringProperty( required = True)
-    chinesename     = db.StringProperty()
-    rank            = db.IntegerProperty()
+    schoolname = db.StringProperty(required=True)
+    chinesename = db.StringProperty()
+    rank = db.IntegerProperty()
     @staticmethod
-    def PutBbsInfo(  bbsinfo ):
+    def PutBbsInfo(bbsinfo):
         if bbsinfo is None: 
             return;
-        
+        itemlist = bbsinfo['itemlist'];
+        if len(itemlist) == 0:
+            return;
         config = bbsinfo['config'];
        
-        qSchool = HighSchoolBbs.gql("WHERE schoolname = :1", config['name'] );
-        if( qSchool.count() > 0 ):
+        qSchool = HighSchoolBbs.gql("WHERE schoolname = :1", config['name']);
+        if(qSchool.count() > 0):
             nschool = qSchool[0];
         else:
-            nschool = HighSchoolBbs( 
-                    schoolname  = unicode(config['name']), 
-                    chinesename = unicode(config['chinesename']),
-                    rank        = config['rank']);
+            nschool = HighSchoolBbs(
+                    schoolname=unicode(config['name']),
+                    chinesename=unicode(config['chinesename']),
+                    rank=config['rank']);
             nschool.put();
     
-        qTopTenItem = TopTenItem.gql('where school = :1',nschool);
+        qTopTenItem = TopTenItem.gql('where school = :1', nschool);
         
         for item in qTopTenItem :
             item.delete();
-        itemlist = bbsinfo['itemlist'];
         
-        for i in range( len(itemlist) ):
+        for i in range(len(itemlist)):
             item = itemlist[i];
-            Titem = TopTenItem( board       = unicode( item['board']),
-                                boardlink   = unicode( item['boardlink'] ) ,
-                                title       = db.Text(item['title']),
-                                titlelink   = item['titlelink'],
-                                author      = unicode( item['author'] ),
-                                authorlink  = unicode( item['authorlink'] ),
-                                postcount   = int(item['postcount']),
-                                order       = i,
-                                school      = nschool);
+            Titem = TopTenItem(board=unicode(item['board']),
+                                boardlink=unicode(item['boardlink']) ,
+                                title=db.Text(item['title']),
+                                titlelink=item['titlelink'],
+                                author=unicode(item['author']),
+                                authorlink=unicode(item['authorlink']),
+                                postcount=int(item['postcount']),
+                                order=i,
+                                school=nschool);
     
             Titem.put();
         
@@ -53,26 +54,26 @@ class HighSchoolBbs(db.Model):
 
 
 class TopTenItem(db.Model):
-    board       = db.StringProperty()
-    boardlink   = db.StringProperty()
-    title       = db.StringProperty( multiline=True)
-    titlelink   = db.StringProperty()
-    author      = db.StringProperty()
-    authorlink  = db.StringProperty()
-    school      = db.ReferenceProperty(HighSchoolBbs, required = True );
-    postcount   = db.IntegerProperty();
-    order       = db.IntegerProperty();
+    board = db.StringProperty()
+    boardlink = db.StringProperty()
+    title = db.StringProperty(multiline=True)
+    titlelink = db.StringProperty()
+    author = db.StringProperty()
+    authorlink = db.StringProperty()
+    school = db.ReferenceProperty(HighSchoolBbs, required=True);
+    postcount = db.IntegerProperty();
+    order = db.IntegerProperty();
     
-class MostTopTenItem( db.Model ):
+class MostTopTenItem(db.Model):
     chinesename = db.StringProperty();
-    title       = db.StringProperty( multiline=True);
-    titlelink   = db.StringProperty();
-    board       = db.StringProperty();
-    author      = db.StringProperty();
-    postcount   = db.IntegerProperty();
+    title = db.StringProperty(multiline=True);
+    titlelink = db.StringProperty();
+    board = db.StringProperty();
+    author = db.StringProperty();
+    postcount = db.IntegerProperty();
     
     @staticmethod
-    def putStaticalResult( itemlist ):
+    def putStaticalResult(itemlist):
         if itemlist is None:
             return;
         old_list = MostTopTenItem.all();
@@ -80,14 +81,15 @@ class MostTopTenItem( db.Model ):
             item.delete();
         for item in itemlist:
             n_result = MostTopTenItem(
-                        chinesename = unicode(item.school.chinesename),
-                        title       = item.title,
-                        titlelink   = item.titlelink,
-                        board       = item.board,
-                        author      = item.author,
-                        postcount   = item.postcount);                        
+                        chinesename=unicode(item.school.chinesename),
+                        title=item.title,
+                        titlelink=item.titlelink,
+                        board=item.board,
+                        author=item.author,
+                        postcount=item.postcount);                        
             n_result.put();
         return;
+    
     
      
     
