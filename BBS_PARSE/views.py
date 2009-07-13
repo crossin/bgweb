5ca962d0822e3908fd0cc8b007c1a56c93d37e36
wallis999 <wallis999@de8bf3d2-1df4-11de-9c9a-2d325a3e6494>
2009-07-13 09:06:29 +0000

    加入了十大推荐文章的函数编写

--- BBS_PARSE/views.py
+++ BBS_PARSE/views.py
@@ -15,7 +15,7 @@ from bbs_settings import *;
 from bbs_parser import *;
 
 
-def respond(request,  template, params=None):
+def respond(request, template, params=None):
   if params is None:
     params = {}   
   if not template.endswith('.html'):
@@ -23,43 +23,67 @@ def respond(request,  template, params=None):
   return shortcuts.render_to_response(template, params)
   
 def freshMostTopTen(): 
+#    bbslist = [];
+#    highschoolbbslist = TopTenItem.all();
+#    highschoolbbslist.order('-postcount');
+#    bstlen = 0;
+#    if  highschoolbbslist.count() < 10 :
+#        bstlen = len(highschoolbbslist);
+#    else :
+#        bstlen = 10;
+#       
+#    results = highschoolbbslist.fetch(bstlen);
+#    MostTopTenItem.putStaticalResult(results);
     bbslist = [];
-    highschoolbbslist = TopTenItem.all();
-    highschoolbbslist.order('-postcount');
-    bstlen = 0;
-    if  highschoolbbslist.count() < 10 :
-        bstlen = len(highschoolbbslist);
-    else :
-        bstlen = 10;
-       
-    results =  highschoolbbslist.fetch( bstlen );
-    MostTopTenItem.putStaticalResult( results );
-       
+    highschoolbbslist = HighSchoolBbs.all();
+    highschoolbbslist.order('rank');
+    highschoollist1 = highschoolbbslist[0:10]
+    for school in  highschoollist1:
+        toptenlist = TopTenItem.gql('where school = :1 ORDER BY order ASC', school);
+
+        bbsinfo = {
+                    'name': unicode(school.schoolname),
+                    'chinesename':unicode(school.chinesename),
+                    'itemlist':toptenlist,
+                    }
+        bbslist.append(bbsinfo);
+    recolist = [];
+    for list in bbslist:
+        recoinfo = list['itemlist'][0];
+#        recoinfo = {
+#                    'boardlink': list['itemlist'][0].boardlink,
+#                    'board': list['itemlist'][0].board,
+#                    'titlelink': list['itemlist'][0].titlelink,
+#                    'title': list['itemlist'][0].title,
+#        }
+        recolist.append(recoinfo);
+    MostTopTenItem.putStaticalResult(recolist);
+    return  recolist;
     
-def freshbbs( ):  
+def freshbbs():  
     b = BBSParser();
-    needXpathBBSList = [ sjtubbs , jlubbs ];
-    needRegularExpressionList1 = [smthbbs, lilybbs, fudanbbs, xjtubbs, njuptbbs ,nankaibbs, newsmth]; 
-    needRegularExpressionList2 = [ ustcbbs , tjbbs, zsubbs, csubbs, dlutbbs, xmubbs ];
+    needXpathBBSList = [ sjtubbs , jlubbs, xmubbs , sdubbs, seubbs, rucbbs, lzubbs, caubbs, ustbbbs, uestcbbs ];
+    needRegularExpressionList1 = [newsmth, zjubbs, lilybbs, fudanbbs]; 
+#    needRegularExpressionList2 = [ustcbbs, zsubbs, whubbs, xjtubbs, scubbs, hitbbs, tjubbs, csubbs, buaabbs, dlutbbs, njuptbbs, bjtubbs, tjbbs];
 
     context = [];
 
     for bbs in needRegularExpressionList1:
-        context.append( b.parsebbsbyRegularExpression( bbs ));
+        context.append(b.parsebbsbyRegularExpression(bbs));
     for bbs in needXpathBBSList:
-        context.append( b.parsebbsbyXpath( bbs ));
+        context.append(b.parsebbsbyXpath(bbs));
   
     for bbsinfoitem in context:
         HighSchoolBbs.PutBbsInfo(bbsinfoitem);
     
     return context;
 
-def freshbbs2( ):  
+def freshbbs2():  
     b = BBSParser();
-    needRegularExpressionList2 = [ ustcbbs , tjbbs, nankaibbs, zsubbs, csubbs, dlutbbs, xmubbs ];
+    needRegularExpressionList2 = [ustcbbs, sysubbs, whubbs, xjtubbs, scubbs, hitbbs, tjubbs, csubbs, buaabbs, dlutbbs, njuptbbs, bjtubbs, tjbbs];
     context = [];
     for bbs in needRegularExpressionList2:
-        context.append( b.parsebbsbyRegularExpression( bbs ));
+        context.append(b.parsebbsbyRegularExpression(bbs));
 
     for bbsinfoitem in context:
         HighSchoolBbs.PutBbsInfo(bbsinfoitem);
@@ -67,21 +91,21 @@ def freshbbs2( ):
     freshMostTopTen();
     return context;
 
-def getFreshBBS2( request ): 
+def getFreshBBS2(request): 
     freshbbs2();
     return HttpResponse("Part2 Updated Successfully")   
     
-def getFreshBBS1( request ):
+def getFreshBBS1(request):
     freshbbs();
     return HttpResponse("Part1 Updated Successfully")
     
 
-def getbbsitemlist( bbsname ):
+def getbbsitemlist(bbsname):
     slist = HighSchoolBbs.gql('where schoolname = :1', bbsname);
     if slist.count() == 0 :
         return;
     school = slist[0];
-    toptenlist = TopTenItem.gql('where school = :1',school);
+    toptenlist = TopTenItem.gql('where school = :1', school);
     bbsinfo = {
                 'name': unicode(school.schoolname),
                 'chinesename':unicode(school.chinesename),
@@ -90,63 +114,81 @@ def getbbsitemlist( bbsname ):
     return bbsinfo;
     
 
-def getbbs( request ):
-    bbsnamelist = ['sjtu','smth2','lily','tongji','fudan','xjtu'];
+def getbbs(request):
+#    bbsnamelist = ['sjtu', 'smth2', 'zju', 'lily', 'fudan', 'ustc', 'zsu', 'whu', 'jlu', 'xjtu', 'scu', 'hit', 'sdu', 'tongji', 'csu', 'seu', 'ruc', 'buaa', 'xmu', 'dlut' ];
+    bbsnamelist = [];
+    highschoollist = HighSchoolBbs.all();
+    count = highschoollist.count();
+    highschoollist.order('rank');
+    for item in highschoollist:
+        bbsnamelist.append(item.schoolname);
     if 'selectedtop1' in request.COOKIES and request.COOKIES['selectedtop1']:
         favbbs = request.COOKIES['selectedtop1'];
         try:
             i = bbsnamelist.index(favbbs)
             bbsnamelist.remove(favbbs)
-            bbsnamelist.insert(0,favbbs);
+            bbsnamelist.insert(0, favbbs);
         except ValueError:
-            bbsnamelist.insert(0,favbbs);
+            bbsnamelist.insert(0, favbbs);
             bbsnamelist.pop();
     bbslist = [];
     
     for name in  bbsnamelist:
-        bbslist.append( getbbsitemlist(name) );
-        
-    if  ( 'card' in request.GET ) : 
-        return render_to_response('list_card.html', {'context':bbslist});
-    
-    return render_to_response('BBS_PARSE.html', {'context':bbslist});
+        bbslist.append(getbbsitemlist(name));
+    if  count >= 20:
+        cardlist = bbslist[0:20];
+    else:
+         cardlist = bbslist;
+    if  ('card' in request.GET) : 
+        return render_to_response('list_card.html', {'context':cardlist});
+    bbstoplist = bbslist[0:10];
+    recolist = MostTopTenItem.all();
+    return render_to_response('BBS_PARSE.html', {'context':bbstoplist, 'recommend':recolist});
 
-def getMostTop10( request ):
-    return redirect('/page/BbsRender.html')
+def getMostTop10(request):
+    return redirect(' / page / BbsRender.html')
     
     
-def getRecommended( request ):
-    bbslist = MostTopTenItem.all().fetch( 10 );       
-    return render_to_response('list_recommend.html', { "context":bbslist}); 
+def getRecommended(request):
+    recolist = MostTopTenItem.all();
+    return render_to_response('list_recommend.html', { "context":recolist}); 
 
 
-def getfullbbslist( request ):
+def getfullbbslist(request):
     bbslist = [];
     highschoollist = HighSchoolBbs.all();
+    count = highschoollist.count();
     highschoollist.order('rank');
-    for school in  highschoollist:
-        toptenlist = TopTenItem.gql('where school = :1 ORDER BY order ASC',school);
+    highschoollist1 = highschoollist[10:count];
+    for school in  highschoollist1:
+        toptenlist = TopTenItem.gql('where school = :1 ORDER BY order ASC', school);
 
         bbsinfo = {
                     'name': unicode(school.schoolname),
                     'chinesename':unicode(school.chinesename),
                     'itemlist':toptenlist,
                     }
-        bbslist.append( bbsinfo);
-   
-    if  ( 'card' in request.GET ) : 
-        return render_to_response('list_card.html', {'context':bbslist});
+        bbslist.append(bbsinfo);
+#    
+#    if  ('card' in request.GET) : 
+#        return render_to_response('list_card.html', {'context':bbslist});
     
-    return render_to_response('BBS_PARSE.html', {'context':bbslist});
+    return render_to_response('BBS_PARSE_LIST.html', {'context':bbslist});
     
     
-def get_bbsnamelist( request):
+def get_bbsnamelist(request):
     highschoollist = HighSchoolBbs.all();
-    total_len =  highschoollist.count();
-    unit_len  = total_len / 3 + 1;
+    count = highschoollist.count();
+    highschoollist.order('rank');
+#    total_len = highschoollist.count();
+    if count >= 20:
+        total_len = 20;
+    else:
+        total_len = count;
+    unit_len = total_len / 3 + 1;
     namelist1 = highschoollist[0:unit_len];
-    namelist2 = highschoollist[unit_len+1:unit_len*2+1];
-    namelist3 = highschoollist[unit_len*2+2:total_len];
+    namelist2 = highschoollist[unit_len + 1:unit_len * 2 + 1];
+    namelist3 = highschoollist[unit_len * 2 + 2:total_len];
     
     return render_to_response('list_bbs.html', {
                                         'namelist1':namelist1,
